586e2a76c178256cba38805dbf851f23b0fecd51
mohamedhanif045 <hanifmohamed045@gmail.com>
2024-09-14 13:08:37 +0300

    fixed lab

--- server/app.py
+++ server/app.py
@@ -6,33 +6,33 @@ app = Flask(__name__)
 def index():
     return '<h1>Python Operations with Flask Routing and Views</h1>'
 
-@app.route('/print/<string:parameter>')
-def print_string(parameter):
-    print(parameter)  # Prints to console
-    return parameter  # Displays in browser
+@app.route("/print/<parameter>")
+def print_parameter(parameter):
+    print(parameter)
+    return parameter
 
-@app.route('/count/<int:parameter>')
-def count(parameter):
-    # Return numbers separated by newline characters
-    return '\n'.join(str(i) for i in range(parameter))
+@app.route("/count/<int:parameter>")
+def count_parameter(parameter):
+    count = f""
+    for i in range(parameter):
+        count += f"{i}\n"
+    return count
 
-@app.route('/math/<int:num1>/<operation>/<int:num2>')
-def math(num1, operation, num2):
-    if operation == '+':
-        result = num1 + num2
-    elif operation == '-':
-        result = num1 - num2
-    elif operation == '*':
-        result = num1 * num2
-    elif operation == '/':
-        if num2 == 0:
-            return 'Division by zero is undefined', 400
-        result = num1 / num2
-    elif operation == '%':
-        result = num1 % num2
+@app.route("/math/<int:parameter1>/<string:operation>/<int:parameter2>")
+def math(operation, parameter1, parameter2):
+    if operation == "+":
+        answer = parameter1 + parameter2
+    elif operation == "-":
+        answer = parameter1 - parameter2
+    elif operation == "*":
+        answer = parameter1 * parameter2
+    elif operation == "div":
+        answer = parameter1 / parameter2
+    elif operation == "%":
+        answer = parameter1 % parameter2
     else:
-        return 'Invalid operation', 400
-    return str(result)
+        return None
+    return str(answer)
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
